ceaf49222153e2458a5844aaf3c67902af7a5878
Pisano <154333911+hugopisano5444@users.noreply.github.com>
2024-07-20 13:05:09 +0200

    Test pre-push hook with passed test

--- routes/palettes.js
+++ routes/palettes.js
@@ -5,7 +5,7 @@ const Palette = require('../models/Palette');
 // Get all palettes
 router.get('/', async (req, res) => {
   try {
-    const palettes = await Palette.find().toArray();
+    const palettes = await Palette.find();
     res.json(palettes);
   } catch (err) {
     res.status(500).json({ message: err.message });
